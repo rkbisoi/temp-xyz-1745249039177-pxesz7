6a0b24d53eef162c19d7e34d73616b955396802d
rkbisoi <158269740+rkbisoi@users.noreply.github.com>
2025-04-21 20:57:34 +0530

    Copy src/data.ts from xyz

---
+++ src/data.ts
@@ -0,0 +1,4 @@
+export const API_URL = 'https://api.intelqe.io'
+export const LLM_WEBSOCKET_URL = 'wss://llm.intelqe.io'
+export const LLM_URL = 'https://llm.intelqe.io'
+export const SELENIUM_URL = 'https://test.selenium.flameai.io/?autoconnect=1&resize=scale&password=secret'
